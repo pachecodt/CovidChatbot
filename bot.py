d2803615f381a4f0c305ca6c23ba1c816b3dd5c9
Felipe Pacheco <ext.filipe.pacheco@farfetch.com>
2021-06-22 20:01:27 +0100

    [Fix] Remove unecessary things

--- bot.py
+++ bot.py
@@ -6,19 +6,12 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 from helper import chatbot_response
 
-
-GREETING_KEYWORDS = ("olá", "oi", "viva", "boas", "hi")
-QUOTES_KEYWORDS = ("quote", "frase", "citação", "citacao")
-CATS_KEYWORDS = ("cat", "gato", "gata", "gatinho", "gatinha")
-
 # Configure application
 app = Flask(__name__)
 
 
 @app.route("/send")
 def send():
-    # Find your Account SID and Auth Token at twilio.com/console
-    # and set the environment variables. See http://twil.io/secure
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
@@ -30,35 +23,6 @@ def send():
     )
 
 @app.route('/bot', methods=['POST'])
-def bot():
-    incoming_msg = request.values.get("Body", "").lower()
-    resp = MessagingResponse()
-    msg = resp.message()
-    responded = False
-
-    if bool([ele for ele in GREETING_KEYWORDS if(ele in incoming_msg)]):
-        msg.body("Hi!" + 3 * "\n")
-        responded = True
-
-    if bool([ele for ele in QUOTES_KEYWORDS if(ele in incoming_msg)]):
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'Não consegui recuperar uma citação neste momento, desculpe.'
-        msg.body(quote)
-        responded = True
-
-    if bool([ele for ele in CATS_KEYWORDS if(ele in incoming_msg)]):
-        msg.media('https://cataas.com/cat')
-        responded = True
-    
-    if not responded:
-        msg.body("Só conheço frases e gatos famosos, desculpe!")
-    return str(resp)
-
-@app.route('/bot2', methods=['POST'])
 def bot2():
     incoming_msg = request.values.get("Body", "").lower()
     resp = MessagingResponse()
